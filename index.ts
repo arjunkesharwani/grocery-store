c8e315ca88101862209e77e2e5205323aa549b00
Arjun Kesharwani <arjun.kesharwani@coditas.com>
2024-06-21 21:44:40 +0530

    process user input to calculate sale and print reciept

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { connectToDatabase } from "./src/config/database";
 import * as readline from "readline";
 import { setupDatabase } from "./src/models";
+import CartService from "./src/services/cart.service"
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -23,6 +24,14 @@ async function main() {
     "Please enter all the items purchased separated by a comma: "
   );
   console.log("User input:", input);
+  const items = input.split(",").map((item) => item.trim().toLowerCase());
+
+  for (const item of items) {
+    await CartService.addItem(item);
+  }
+
+  await CartService.printReceipt();
+  await CartService.clearCart();
 }
 
 main();
